168030c24791a03e0b0ba8de83bf65cd77f54513
Anurag Naik <anurag.naiik@gmail.com>
2023-10-21 19:59:50 +0530

    added for loop

--- first_script.sh
+++ first_script.sh
@@ -22,10 +22,19 @@ else
 fi
 '
 ##############################
-count=0
+: 'count=0
 read numb
 while [ $count -lt $numb ]
 do
 	count=`expr $count + 1`
 	echo "This line is being printed for $count time!!"
 done
+'
+##############################
+
+for i in 1 2 3 4 5 6
+do
+	echo $(( i*2 ))
+done
+
+
